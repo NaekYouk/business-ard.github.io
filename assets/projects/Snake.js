3956b2a94aad1706f2a138ab8394311ab628a37a
NaekYouk <55960361+NaekYouk@users.noreply.github.com>
2019-09-30 00:07:42 +0300

    Update Snake.js

--- assets/projects/Snake.js
+++ assets/projects/Snake.js
@@ -184,6 +184,54 @@ $("body").keydown(function (event) {
     }
 });
 
+// SWIPE
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            snake.setDirection("left");
+        } else {
+            snake.setDirection("right");
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            snake.setDirection("up");
+        } else { 
+            snake.setDirection("down");
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+
 let snake = new Snake();
 let apple = new Apple();
 let interval = 110;
